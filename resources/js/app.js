ff76abe19b396702bf213a650138218f4d90fbf1
Manuel Gomez <jmgc@virket.com>
2023-04-26 19:55:52 -0600

    creacion de notificaciones broadcast privado

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,25 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+// privado
+Echo.private('notifications')
+    .listen('UserSessionChanged', (e) => {
+        const notificationElement = document.getElementById('notification');
+        notificationElement.innerText = e.message;
+        notificationElement.classList.remove('invisible');
+        notificationElement.classList.remove('alert-success');
+        notificationElement.classList.remove('alert-danger');
+        notificationElement.classList.add('alert-' + e.type);
+    });
+
+// publico
+// Echo.channel('notifications')
+//     .listen('UserSessionChanged', (e) => {
+//         const notificationElement = document.getElementById('notification');
+//         notificationElement.innerText = e.message;
+//         notificationElement.classList.remove('invisible');
+//         notificationElement.classList.remove('alert-success');
+//         notificationElement.classList.remove('alert-danger');
+//         notificationElement.classList.add('alert-' + e.type);
+//     });
